23cad72cf74d86f5371f71d41a98d37680ee0b6f
DevanshuSinghai <devanshusinghai2005@gmail.com>
2024-03-02 18:07:53 +0530

    added function to restart

--- maze.py
+++ maze.py
@@ -1,12 +1,10 @@
 import pygame
 import sys
-import random
-import math
 
 # Constants
 WIDTH, HEIGHT = 800, 600
 WHITE = (255, 255, 255)
-COL = (123,111,100)
+COL = (123, 111, 100)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREY = (169, 169, 169)  # Grey color for roads
@@ -20,7 +18,7 @@ pygame.display.set_caption("Map with Roads and Ball")
 clock = pygame.time.Clock()
 
 # Define road areas (x, y, width, height)
-roads = [
+original_roads = [
     (100, 200, 600, 40),
     (300, 100, 40, 400),
     (500, 300, 200, 40),
@@ -37,6 +35,9 @@ ball_velocity = 5
 start_time = pygame.time.get_ticks()
 display_time = 5000  # 5 seconds in milliseconds
 
+# Player status
+player_out = False
+
 # Main game loop
 running = True
 display_complete = False
@@ -47,36 +48,36 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    # Move the ball
-    if keys[pygame.K_RIGHT]:
-        ball_x += ball_velocity
-    elif keys[pygame.K_LEFT]:
-        ball_x -= ball_velocity
-    elif keys[pygame.K_DOWN]:
-        ball_y += ball_velocity
-    elif keys[pygame.K_UP]:
-        ball_y -= ball_velocity
-
-    # Check if the ball touches any road
-    for road in roads:
-        road_rect = pygame.Rect(road)
-        ball_rect = pygame.Rect(ball_x - BALL_RADIUS, ball_y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
-        if road_rect.colliderect(ball_rect):
-            # Reset ball position to start
-            ball_x = 30
-            ball_y = 30
-
     # Check if display time is over
     current_time = pygame.time.get_ticks()
     if current_time - start_time >= display_time:
         display_complete = True
+
+    # Move the ball only when the screen turns black and the player is not out
+    if display_complete and not player_out:
+        if keys[pygame.K_RIGHT]:
+            ball_x += ball_velocity
+        elif keys[pygame.K_LEFT]:
+            ball_x -= ball_velocity
+        elif keys[pygame.K_DOWN]:
+            ball_y += ball_velocity
+        elif keys[pygame.K_UP]:
+            ball_y -= ball_velocity
+
+    # Check if the ball touches any road
+    for road in original_roads:
+        road_rect = pygame.Rect(road)
+        ball_rect = pygame.Rect(ball_x - BALL_RADIUS, ball_y - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
+        if road_rect.colliderect(ball_rect) and display_complete:
+            # Player is out if hits the road
+            player_out = True
 
     # Draw background
     screen.fill(BLACK if display_complete else COL)
 
     # Draw roads if display time is not over
     if not display_complete:
-        for road in roads:
+        for road in original_roads:
             pygame.draw.rect(screen, GREY, road)
 
         # Draw starting and ending points
@@ -89,8 +90,28 @@ while running:
         end_text = font.render("End", True, WHITE)
         screen.blit(end_text, (WIDTH - 100, HEIGHT - 50))
 
-    # Draw ball
-    pygame.draw.circle(screen, WHITE, (ball_x, ball_y), BALL_RADIUS)
+    # Draw ball if the player is not out
+    if not player_out:
+        pygame.draw.circle(screen, WHITE, (ball_x, ball_y), BALL_RADIUS)
+
+    # Display "Player is out" if the player hits a grey road
+    if player_out:
+        font_out = pygame.font.SysFont(None, 64)
+        out_text = font_out.render("Player is out", True, WHITE)
+        screen.blit(out_text, (WIDTH // 2 - 200, HEIGHT // 2))
+        ball_x = 30
+        ball_y = 30
+        restart_text = font_out.render("Press R to restart", True, WHITE)
+
+
+        screen.blit(restart_text, (WIDTH // 2 - 200, HEIGHT // 2 + 50))
+
+        # Check for restart option
+        if keys[pygame.K_r]:
+            player_out = False
+            start_time = pygame.time.get_ticks()
+            display_complete = False
+
 
     pygame.display.flip()
     clock.tick(FPS)
